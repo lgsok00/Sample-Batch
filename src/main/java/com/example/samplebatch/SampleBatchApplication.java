11b45ed181ec86821822e5acea9dafe495846a60
lgsok00 <lgsok00@naver.com>
2024-07-30 18:23:49 +0900

    API 기반, 스케줄 기반 배치 실행

--- src/main/java/com/example/samplebatch/SampleBatchApplication.java
+++ src/main/java/com/example/samplebatch/SampleBatchApplication.java
@@ -2,8 +2,10 @@ package com.example.samplebatch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling  // 스케줄링 활성화
 public class SampleBatchApplication {
 
   public static void main(String[] args) {
---
+++ src/main/java/com/example/samplebatch/schedule/FirstSchedule.java
@@ -0,0 +1,41 @@
+package com.example.samplebatch.schedule;
+
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.configuration.JobRegistry;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+// 스케줄 기반 배치
+@Configuration
+public class FirstSchedule {
+
+  private final JobLauncher jobLauncher;
+  private final JobRegistry jobRegistry;
+
+
+  public FirstSchedule(JobLauncher jobLauncher, JobRegistry jobRegistry) {
+    this.jobLauncher = jobLauncher;
+    this.jobRegistry = jobRegistry;
+  }
+
+  // 스케줄링 메서드
+  @Scheduled(cron = "10 * * * * *", zone = "Asia/Seoul")
+  public void runFirstJob() throws Exception {
+
+    System.out.println("first schedule start");
+
+    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    String date = dateFormat.format(new Date());
+
+    JobParameters jobParameters = new JobParametersBuilder()
+            .addString("date", date)
+            .toJobParameters();
+
+    jobLauncher.run(jobRegistry.getJob("firstJob"), jobParameters);
+  }
+}
